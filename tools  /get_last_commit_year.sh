2c9e5502238e142c04b3c1b588f02b370b6cc716
Yvan-56 <59482621+Yvan-56@users.noreply.github.com>
2026-05-26 16:46:21 +0200

    Create get_last_commit_year.sh

---
+++ tools  /get_last_commit_year.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VKDT_SRC_DIR=$(dirname "$0")
+VKDT_SRC_DIR=$(cd "$VKDT_SRC_DIR/../" && pwd -P)
+
+LAST_COMMIT_YEAR=$(git --git-dir="${VKDT_SRC_DIR}/.git" log -n1 --pretty=%ci)
+
+if [ $? -eq 0 ]; then
+  echo "${LAST_COMMIT_YEAR}" | cut -b 1-4
+  exit 0
+fi
+
+# fallback in case git above failed
+
+date -u "+%Y"
